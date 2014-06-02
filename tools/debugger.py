4942dc01316187aa39452702dae11fb6d5ce837c
Netix (Espinet François) <dev.espinetfrancois@gmail.com>
2014-06-02 10:08:08 +0200

    Added debugging module

---
+++ tools/debugger.py
@@ -0,0 +1,22 @@
+"""Module to debug python programs"""
+
+import sys
+import traceback
+
+def getAllStacks():
+    code = []
+    for threadId, stack in sys._current_frames().iteritems():
+        code.append("\n# ThreadID: %s" % threadId)
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename,
+                                                        lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    return code
+
+def strStacks():
+    out = "\n*** STACKTRACE - START ***\n"
+    out += "".join(getAllStacks())
+    out += "\n*** STACKTRACE - END ***\n"
+    return out
+
